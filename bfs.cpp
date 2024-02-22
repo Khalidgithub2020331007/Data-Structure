c92391076dc0a4d4571cea147cf237e530b19f5c
Khalidgithub2020331007 <107164227+Khalidgithub2020331007@users.noreply.github.com>
2024-02-22 15:07:23 +0600

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,51 +1,79 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-// Function to perform Depth First Search
-void dfs(vector<vector<int>>& adjList, int startNode) {
-    vector<bool> visited(adjList.size(), false); // To keep track of visited nodes
-    stack<int> s; // Stack for DFS
-
-    // Push the start node onto the stack
-    s.push(startNode);
-
-    while (!s.empty()) {
-        int current = s.top(); // Get the top of the stack
-        s.pop(); // Remove the top node from the stack
-
-        if (!visited[current]) {
-            cout << current << " "; // Process current node
-            visited[current] = true; // Mark current node as visited
-
-            // Traverse adjacent nodes of current node
-            for (int adjNode : adjList[current]) {
-                if (!visited[adjNode]) {
-                    s.push(adjNode); // Push adjacent node onto the stack
-                }
-            }
-        }
-    }
+vector<int>adj[30000];
+vector<int>w[30000];
+bool vis[30000];
+int d[30000];
+void clr(int n)
+{
+  int i;
+  for(i=0;i<n;i++)
+  {
+    vis[i]=0;
+    d[i]=0;
+  }
 }
-
-int main() {
-    // Example graph represented using adjacency lists
-    int numNodes = 6;
-    vector<vector<int>> adjList(numNodes);
-
-    // Add edges
-    adjList[0] = {1, 2};
-    adjList[1] = {0, 3, 4};
-    adjList[2] = {0, 4};
-    adjList[3] = {1, 5};
-    adjList[4] = {1, 2, 5};
-    adjList[5] = {3, 4};
-
-    // Start DFS from node 0
-    cout << "DFS Traversal: ";
-    dfs(adjList, 0);
-
-    return 0;
+void bfs(int node,int n)
+{
+  queue<int>q;
+  vis[node]=1;
+  q.push(node);
+  vis[node]=1;
+  while(!q.empty())
+  {
+    node=q.front();
+    q.pop();
+    for(int it=0;it<adj[node].size();it++)
+    {
+      int v=adj[node][it];
+      if(!vis[v])
+      {
+        vis[v]=1;
+        d[v]=d[node]+w[node][it];
+        q.push(v);
+      }
+    }
+  }
+}
+int main(){
+  int t,k;
+  cin>>t;
+  for(k=1;k<=t;k++)
+  {
+    int n,i;
+    cin>>n;
+    for(i=0;i<n;i++)
+    {
+      adj[i].clear();
+      w[i].clear();
+    }
+    for(i=0;i<n-1;i++)
+    {
+      int x,y,c;
+      cin>>x>>y>>c;
+      adj[x].push_back(y);
+      adj[y].push_back(x);
+      w[x].push_back(c);
+      w[y].push_back(c);
+    }
+    int m=0,fn;
+    clr(n);
+    bfs(0,n);
+    for(i=0;i<n;i++)
+    {
+      if(d[i]>m)
+      {
+        m=d[i];
+        fn=i;
+      }
+    }
+    clr(n);
+    int ans=0;
+    bfs(fn,n);
+    for(i=0;i<n;i++)
+    {
+      if(d[i]>ans)ans=d[i];
+    }
+    cout<<"Case"<<" "<<k<<": "<<ans<<endl;
+  }
 }
